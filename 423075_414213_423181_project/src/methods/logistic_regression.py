1eaa7ed0c15f1e499d205b4c3f22634a6a87c397
Rachel <rlewis2@andrew.cmu.edu>
2026-04-23 19:52:54 +0200

    UNTESTED - logistic regression implementation

--- 423075_414213_423181_project/src/methods/logistic_regression.py
+++ 423075_414213_423181_project/src/methods/logistic_regression.py
@@ -2,39 +2,86 @@ import numpy as np
 
 from ..utils import get_n_classes, label_to_onehot, onehot_to_label
 
+# filled in by Rachel Lewis
+
+def m_mult(m1, m2):
+    # matrix multiplication
+    return np.dot(m1, m2)
+
+
+def m_T(m):
+    # matrix transpose
+    return np.transpose(m)
+
+
+def m_exp(m):
+    # elementwise exponential
+    return np.exp(m)
+
 
 class LogisticRegression(object):
     """
-    Logistic regression classifier.
+    Multi-class logistic regression classifier.
     """
 
     def __init__(self, lr, max_iters=500):
-        """
-        Initialize the new object (see dummy_methods.py)
-        and set its arguments.
-
-        Arguments:
-            lr (float): learning rate of the gradient descent
-            max_iters (int): maximum number of iterations
-        """
         self.lr = lr
         self.max_iters = max_iters
+        self.W = None           # weights
+        self.C = None           # num of classes
 
+
+    # from softmax formula in Lec4 slide 47
+    def softmax(self, scores):
+        """
+        scores: shape (N, C)
+        returns probabilities of shape (N, C)
+        """
+        # prevent overflow from large exp() nums: subtract row-wise max
+        scores = scores - np.max(scores, axis=1, keepdims=True)
+        exp_scores = np.exp(scores)
+
+        # normalization to make probabilities over the row
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        return probs
+    
+    # from Lec 4 "Multi-class logistic regression"
     def fit(self, training_data, training_labels):
         """
         Trains the model, returns predicted labels for training data.
 
         Arguments:
             training_data (np.array): training data of shape (N,D)
-            training_labels (np.array): regression target of shape (N,)
+            training_labels (np.array): class labels of shape (N,)
         Returns:
-            pred_labels (np.array): target of shape (N,)
+            pred_labels (np.array): predicted labels of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        
+        N, D = training_data.shape
+        self.C = get_n_classes(training_labels)
+
+        # one-hot labels, shape (N, C)
+        Y = label_to_onehot(training_labels, self.C)
+
+        # random initialization of W
+        self.W = 0.01 * np.random.randn(D, self.C)
+
+        for i in range(self.max_iters):
+            # Gradient descent process: Lec 4 slide 49
+
+            # X*W aka (w_k^T * x) in lecture softmax func
+            scores = m_mult(training_data, self.W)
+            Y_hat = self.softmax(scores)
+
+            # gradient from lec 4 slide 51
+            # ∇_W R(W) = sum_i x_i (y_hat_i - y_i)^T
+            # vectorized: X^T (Y_hat - Y)
+            grad = m_mult( m_T(training_data), (Y_hat - Y) )
+
+            self.W = self.W - self.lr * grad
+
+        # return predictions on training data
+        pred_labels = self.predict(training_data)
         return pred_labels
 
     def predict(self, test_data):
@@ -46,9 +93,12 @@ class LogisticRegression(object):
         Returns:
             pred_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        # class scores
+        scores = m_mult(test_data, self.W)
+
+        # probabilities
+        Y_hat = self.softmax(scores)
+
+        # pick class with highest probability
+        pred_labels = onehot_to_label(Y_hat)
         return pred_labels
