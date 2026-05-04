07bea9f3394dc9e80c7f83231f8483bf3be66e31
Rachel <rlewis2@andrew.cmu.edu>
2026-05-04 11:28:23 +0200

    creating updated Milestone-1 and 2 folders

--- Milestone-1/423075_414213_423181_project/main.py
+++ Milestone-1/423075_414213_423181_project/main.py
@@ -70,7 +70,14 @@ def main(args):
 
     train_features = (train_features - mean) / std
     test_features = (test_features - mean) / std
-
+    means = train_features.mean(axis=0, keepdims=True)
+    
+    stds  = train_features.std(axis=0, keepdims=True)
+    stds[stds == 0] = 1.0
+    
+    train_features = normalize_fn(train_features, means, stds)
+    test_features  = normalize_fn(test_features,  means, stds)
+    
     ## 3. Initialize the method you want to use.
 
     # Follow the "DummyClassifier" example for your methods
@@ -79,6 +86,7 @@ def main(args):
 
     elif args.method == "knn":
         ### WRITE YOUR CODE HERE
+        method_obj = KNN(k=args.K, task_kind=args.task)
         pass
 
     elif args.method == "logistic_regression":
--- Milestone-1/423075_414213_423181_project/src/methods/knn.py
+++ Milestone-1/423075_414213_423181_project/src/methods/knn.py
@@ -12,6 +12,8 @@ class KNN(object):
         """
         self.k = k
         self.task_kind = task_kind
+        self.training_data = None
+        self.training_labels = None
 
     def fit(self, training_data, training_labels):
         """
@@ -29,11 +31,10 @@ class KNN(object):
             pred_labels (np.array): labels of shape (N,)
         """
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        self.training_data = training_data
+        self.training_labels = training_labels
+
+        pred_labels = self.predict(training_data)
         return pred_labels
 
     def predict(self, test_data):
@@ -45,9 +46,36 @@ class KNN(object):
         Returns:
             test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
-        return test_labels
+        predicted_values = [self._predict_single_point(point) for point in test_data]
+        return np.array(predicted_values)
+
+    def _find_distances(self, point):
+        # sqrt not needed since just the closest points need to be found
+        return np.sum((self.training_data - point) ** 2, axis=1)
+
+    def _predict_single_point(self, point):
+        distances = self._find_distances(point)
+        knn_idx = np.argsort(distances)[:self.k]
+        knn_labels = self.training_labels[knn_idx]
+
+        if self.task_kind == "regression":
+            return np.mean(knn_labels)
+
+        elif self.task_kind == "classification":
+            knn_labels = knn_labels.astype(int)
+            labels, counts = np.unique(knn_labels, return_counts=True)
+            mode = np.max(counts)
+            corresponding_labels = labels[counts == mode]
+
+            if len(corresponding_labels) == 1:
+                return corresponding_labels[0]
+
+            else:
+                best_label = None
+                best_dist = None
+                for label in corresponding_labels:
+                    avg_dist = np.mean(distances[knn_idx][knn_labels == label])
+                    if best_dist is None or avg_dist < best_dist:
+                        best_dist = avg_dist
+                        best_label = label
+                return best_label
