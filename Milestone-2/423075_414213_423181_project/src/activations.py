f4a82c7b5967eff3b5a740d7b747ff5fb9c394b1
Rachel <rlewis2@andrew.cmu.edu>
2026-05-06 12:48:55 +0200

    added activation forward + gradient functions

--- Milestone-2/423075_414213_423181_project/src/activations.py
+++ Milestone-2/423075_414213_423181_project/src/activations.py
@@ -3,17 +3,24 @@ import numpy as np
 class Sigmoid:
     @staticmethod
     def forward(z):
-        return ...
+        # as defined in Lec 9 slide 24
+        return 1/(1 + np.exp(-z))
 
     @staticmethod
     def gradient(z):
-        return ...
+        # easily found by taking derivative of forward function wrt z
+        return (np.exp(-z)) / ( (1 + np.exp(-z))^2 )
 
 class ReLU:
     @staticmethod
     def forward(z):
-        return ...
+        # as defined in Lec 9 slide 24
+        return np.max(z, 0)
 
     @staticmethod
     def gradient(z):
-        return ...
+        return (int)(z > 0)  
+        # basically, if z > 0 , that means on the forward ReLU passed the value
+        # aka the gradient is 1
+        # but if z == 0, that means ReLu didn't activate on the forward, 
+        # so gradient 0
