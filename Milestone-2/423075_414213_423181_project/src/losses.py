b3e93506274a85b8b96f00afa1c04444ff89cb92
Rachel <rlewis2@andrew.cmu.edu>
2026-05-07 12:28:46 +0200

    gradient funct for cross entropy

--- Milestone-2/423075_414213_423181_project/src/losses.py
+++ Milestone-2/423075_414213_423181_project/src/losses.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MSE:
     @staticmethod
     def loss(y_true, y_pred):
@@ -6,9 +8,27 @@ class MSE:
         :param y_pred: (array) Prediction vector
         :return: (flt)
         """
-        ### WRITE YOUR CODE HERE
+        # as define in Lec 2 slide 42
+        return np.mean((y_pred - y_true) ** 2)
 
     @staticmethod
     def gradient(y_true, y_pred):
-        ### WRITE YOUR CODE HERE
-        return
+        # simple deriv of the MS loss func
+        return 2 * (y_pred - y_true)
+    
+class CrossEntropy:
+    @staticmethod
+    def loss(y_true, y_pred):
+        """
+        :param y_true: (array) One hot encoded truth vector.
+        :param y_pred: (array) Prediction vector
+        :return: (flt)
+        """
+        # from Lec 4 slide 48
+        return -np.sum( y_true * np.log(y_pred) )
+
+    @staticmethod
+    def gradient(y_true, y_pred):
+        # derivative of the above function wrt y_pred
+        # not a sum because we are making gradient vector
+        return - y_true / y_pred
